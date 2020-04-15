a6f9473e56ebfb80d617d63e2b59dc13cf2ddd26
Zoey Chesny <z.chesny@gmail.com>
2020-04-15 20:17:27 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,12 +2,12 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  # include Dance
-  extend MetaDancing
-  #
-  # attr_accessor :name
-  #
-  # def initialize(name)
-  #   @name = name
-  # end
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
 end
