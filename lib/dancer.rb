a9d741db33ee5b1798b472d54f01a3b0b5a06fcb
Zoey Chesny <z.chesny@gmail.com>
2020-04-15 20:17:09 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,6 +2,17 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name 
+  end
+end
+
+class Dancer
   # include Dance
   extend MetaDancing
   #
