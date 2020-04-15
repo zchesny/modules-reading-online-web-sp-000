8ff2d7b51431086bd7ff3b7e8955615c6d598584
Zoey Chesny <z.chesny@gmail.com>
2020-04-15 19:47:44 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,12 +2,12 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  include Dance
+  # include Dance
   extend MetaDancing
-
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
+  #
+  # attr_accessor :name
+  #
+  # def initialize(name)
+  #   @name = name
+  # end
 end
