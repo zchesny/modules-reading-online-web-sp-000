9d8335d217a33d9da686708108baae1e8938bd33
Zoey Chesny <z.chesny@gmail.com>
2020-04-15 19:47:26 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,9 +4,9 @@ require_relative './class_methods_module.rb'
 class Kid
   # include Dance
   extend MetaDancing
-  # 
+  #
   # attr_accessor :name
-  # 
+  #
   # def initialize(name)
   #   @name = name
   # end
